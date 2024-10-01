0a20ba72bdb2e5706de363a1c1fc6d5c96f69851
Jakob spiel <jako5457@gmail.com>
2024-10-01 08:12:13 +0200

    Add project files.

---
+++ TodoLibrary/TodoRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoLibrary.Objects;
+
+namespace TodoLibrary
+{
+    public class TodoRepository
+    {
+
+        private readonly List<TodoItem> _items;
+
+        public TodoRepository() 
+        {
+            _items = new List<TodoItem>() {
+                new TodoItem()
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Created = DateTime.Now,
+                    IsCompleted = false,
+                }
+            };
+        }
+
+        public List<TodoItem> GetTodoItems() => _items;
+
+        public void Add(TodoItem item) => _items.Add(item);
+
+    }
+}
